81eea034870ecaabc91f9ddad0e55e7a8a1d14a5
Flame <yaolynzc@126.com>
2016-09-08 16:00:49 +0800

    first submit

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/express-session/index.d.ts" />
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/mongodb/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/redis/index.d.ts" />
