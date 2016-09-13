a68706633a97d2cbcb929d1c86aad52e7c8fbd4a
Flame <yaolynzc@126.com>
2016-09-13 15:33:00 +0800

    modify app

--- src/common/config/view.js
+++ src/common/config/view.js
@@ -6,7 +6,7 @@ export default {
   type: 'nunjucks',
   content_type: 'text/html',
   file_ext: '.html',
-  file_depr: '/',
+  file_depr: '_',
   root_path: think.ROOT_PATH + '/view',
   adapter: {
     nunjucks: {}
